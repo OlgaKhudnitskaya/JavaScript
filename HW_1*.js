a23a3144f4edc5568e57b5fe2d7f3034195ab509
OlgaKhudnitskaya <105886502+OlgaKhudnitskaya@users.noreply.github.com>
2022-06-21 18:31:42 +0300

    create HW with *

---
+++ HW_1*.js
@@ -0,0 +1,22 @@
+ //1*:
+//Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+//Пример: const checkAge = function(age) {
+//Ваши преобразования
+//}
+age_2 = 18;
+age_3 = 60;
+checkAge = function(age) {
+if (age < age_2) {
+    console.log("You don’t have access cause your age is" + " " + age + " It’s less then");
+} else if (age >= age_2 && age < age_3) {
+    console.log("Welcome  !");
+} else if (age > age_3) {
+    console.log("Keep calm and look Culture channel");
+} else {
+    console.log("Technical work");  
+}
+}
+//Вывести в консоль результат работы функции с возрастами 17, 18, 61
+checkAge(17)
+checkAge(18)
+checkAge(61)
