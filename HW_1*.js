6a561e90b6da505077ca8a6f47786e920b33a1c1
OlgaKhudnitskaya <105886502+OlgaKhudnitskaya@users.noreply.github.com>
2022-06-21 19:33:05 +0300

    Update HW_1*.js

--- HW_1*.js
+++ HW_1*.js
@@ -20,3 +20,25 @@ if (age < age_2) {
 checkAge(17)
 checkAge(18)
 checkAge(61)
+
+//2*:
+//Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+age_2 = 18;
+age_3 = 60;
+checkAge = function(age) {
+ if (typeof age =='number') {
+  if (age < age_2) {
+    console.log("You don’t have access cause your age is" + " " + age + " It’s less then");
+  } else if (age >= age_2 && age < age_3) {
+    console.log("Welcome  !");
+  } else if (age > age_3) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");  
+  }
+} else console.log("Error")
+}
+checkAge(17)
+checkAge(18)
+checkAge(61)
+checkAge('five')
